9724fa98e78d769ea2c05602558410d0f2eb2ef5
modsgamer <rafiqshahzaib681@gmail.com>
2025-05-06 02:19:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'SUFIYAN',
+  author: 'SUFIYAN',
+  botName: "SUFI Bot",
+  botOwner: 'Sufi_Code', // Your name
+  ownerNumber: '923160204461', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
